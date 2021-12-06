1a71193d9922167347414f157f707a775f559fd3
Zach Fogg <z.fogg1234@gmail.com>
2021-12-06 09:55:49 -0500

    Initial Commit

---
+++ HCI_Prototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HCI_Prototype
+//
+//  Created by Zach Fogg on 11/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
